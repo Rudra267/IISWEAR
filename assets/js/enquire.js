f562d45849d1fa13efdf2c7171aad3b165c27d6f
user.name:"Rudra267 <nrudra550@gmail.com>
2025-08-19 21:30:43 +0530

    update

--- assets/js/enquire.js
+++ assets/js/enquire.js
@@ -1,9 +1,53 @@
   const nameInput = document.getElementById("name");
     const phoneInput = document.getElementById("phone");
     const emailInput = document.getElementById("email");
+    const jobOption = document.getElementById("jobOption");
 
     function sendEmail() {
-        if (!nameInput.value || !phoneInput.value || !emailInput.value) {
+     if(jobOption){
+
+        if (!nameInput.value || !phoneInput.value || !emailInput.value || !jobOption.value) {
+            Swal.fire({
+                position: "center",
+                icon: "error",
+                title: `Please fill out all required fields`,
+                showConfirmButton: false,
+                timer: 1500
+            });
+        } else {
+            const bodyMessage = `
+                Name: ${nameInput.value}
+                Phone: ${phoneInput.value}
+                Email: ${emailInput.value}
+                Selected Option: ${jobOption.value}
+            `;
+
+            const param = {
+                title: "Master Class Form",
+                to_name: "iiSWEAR_Masterclass Form",
+                from_name: "iiSWEAR_Customer Service Team",
+                message: bodyMessage
+            };
+
+             const service_Id = "service_dwlubzp";
+            const template_Id = "template_xbo0b1r";
+
+            emailjs.send(service_Id, template_Id, param)
+                .then((res) => {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: `We will reach out to contact you`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+
+                    document.getElementById("contact-form").reset();
+                })
+                .catch((err) => console.log(err));
+        }
+     }else{
+                if (!nameInput.value || !phoneInput.value || !emailInput.value) {
             Swal.fire({
                 position: "center",
                 icon: "error",
@@ -42,4 +86,5 @@
                 })
                 .catch((err) => console.log(err));
         }
+     }
     }
